36f3424583668e7d3c90340f613e30c6eeb439cf
Jose Maza <mazajose@gmail.com>
2013-07-13 23:55:51 +0000

    modified web.js to include index

--- web.js
+++ web.js
@@ -2,12 +2,12 @@ var express = require('express');
 var fs = require('fs');
 var buf = new Buffer(35);
 
-fs.readFileSync("index.html", buf);
+fs.readFileSync("index.html", "buf");
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString('utf-8'));
+  response.send(buf.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
